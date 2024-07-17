9e0cd65726c572a7c3afcc25bcd94123e64f4b42
brian <liar88828@gmail.com>
2024-07-17 12:36:37 +0700

    save surface

---
+++ app/src/main/java/com/tutor/tutor_jetpack_compose/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.tutor.tutor_jetpack_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.tutor.tutor_jetpack_compose.ui.theme.TutorjetpackcomposeTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			TutorjetpackcomposeTheme {
+				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+					Greeting(
+						name = "Android",
+						modifier = Modifier.padding(innerPadding)
+					)
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun FirstApp(modifier: Modifier = Modifier) {
+	Surface(modifier = modifier.fillMaxSize()) {
+		Column {
+			TestComp(text = "Hello ")
+			TestComp(text = "Hello World")
+			TestComp(text = "Hello World twice")
+		}
+	}
+}
+
+@Composable
+fun TestComp(modifier: Modifier = Modifier, text: String) {
+	Text(text = "Please Subscribe $text", fontSize = 32.sp)
+	HorizontalDivider()
+}
+
+@Preview(showBackground = true, name = "FirstApp")
+@Composable
+private fun FirstAppPrev() {
+	FirstApp()
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+	TutorjetpackcomposeTheme {
+		Greeting("Android")
+	}
+}
